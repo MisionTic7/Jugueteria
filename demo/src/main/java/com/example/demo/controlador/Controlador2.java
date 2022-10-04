3c49272ea1da00fdf3340ad0a834635f9f118df3
Juanito1220 <proyectosoftwarejp@gmail.com>
2022-10-03 20:06:48 -0500

    Prueba

--- demo/src/main/java/com/example/demo/controlador/Controlador2.java
+++ demo/src/main/java/com/example/demo/controlador/Controlador2.java
@@ -1,7 +1,6 @@
 package com.example.demo.controlador;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.demo.interfaceService.IpersonaService;
 import com.example.demo.interfaceService.IproductoService;
-import com.example.demo.modelo.Persona;
+
 import com.example.demo.modelo.Producto;
 public class Controlador2 {
 	@Controller
